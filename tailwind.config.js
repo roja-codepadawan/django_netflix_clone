fa9cd449f3467c53cf22bcc19b303e6e5382b799
Ron <django@email.com>
2024-04-06 15:28:01 +0000

    nav dropdwon conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html', // Pfade zu HTML-Dateien im 'templates'-Verzeichnis
-    './static/admin/js/**/*.js',    // Pfade zu JS-Dateien im 'static'-Verzeichnis
+    "./templates/**/*.html", // Pfade zu HTML-Dateien im 'templates'-Verzeichnis
+    "./static/admin/js/**/*.js", // Pfade zu JS-Dateien im 'static'-Verzeichnis
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "glass-white": "rgba(255, 255, 255, 0.04)",
+        "glass-border": "rgba(255, 255, 255, 0.21)",
+      },
+      boxShadow: {
+        glass: "0 4px 30px rgba(0, 0, 0, 0.1)",
+      },
+      backdropFilter: {
+        blur: "blur(3.9px)",
+      },
+      borderRadius: {
+        glass: "16px",
+      },
+      borderWidth: {
+        glass: "1px",
+      },
+      backdropFilter: ["hover", "focus"], // aktiviert backdrop-filter Klassen
+    },
   },
+  variants: {},
   plugins: [],
-}
+};
 
